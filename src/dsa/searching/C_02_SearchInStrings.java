1681cb6342e4c53d876999156d37b86244992b84
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2025-06-21 23:20:24 +0530

    Adding Searching concept in Strings object creating methods search(), and searchChar()

--- src/dsa/searching/C_02_SearchInStrings.java
+++ src/dsa/searching/C_02_SearchInStrings.java
@@ -1,7 +1,43 @@
 package dsa.searching;
 
+import java.sql.Statement;
+
 public class C_02_SearchInStrings {
     public static void main(String[] args) {
 
+        // creating variable
+        String name = "Sriyansh";
+        // calling both the functions
+
+        System.out.println(search(name,'c'));
+        System.out.println(searchChar(name,'s'));
+
+    }
+    // search function with traditional for loop.
+    static boolean search(String str,char target){
+        if(str.length()==0){
+            return false;
+        }
+
+        for (int i=0; i<str.length();i++){
+            if(target==str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // search function with enhanced for loop.
+    static boolean searchChar(String str,char target){
+        if(str.length()==0){
+            return false;
+        }
+
+        for(char ch:str.toCharArray()){
+            if(target==ch){
+                return true;
+            }
+        }
+        return false;
     }
 }
