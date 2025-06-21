bdc8f306eaed450028647fe8ccc57542190d1af5
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2025-06-21 12:55:41 +0530

    Handling Exceptions in Question-01 of maximumValueInIndex() method and making is more robust.

--- src/dsa/arrays/Q_01_MaximumValueInArray.java
+++ src/dsa/arrays/Q_01_MaximumValueInArray.java
@@ -18,7 +18,20 @@ public class Q_01_MaximumValueInArray {
         System.out.println("The maximum value of element in range is: "+ maxInRange);
 
         // simple finding in range in one statement.
+
         System.out.println(maximumValueInRange(arr,2,4));
+
+        // wrong finding with exception handling.
+        try{
+            int result = maximumValueInRange(arr,2,3);
+            System.out.println(result);
+        }catch(IllegalArgumentException e){
+            System.out.println("Please check the Arguments you are Passing, array should not be empty, and start index should be smaller then end Index.");
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Start or end index out of bounds. Please Check the indexes you are passing.");
+        }catch (Exception e){
+            System.out.println("Something went wrong. Please check !!!");
+        }
     }
 
     // write a function which returns maximum value of any integer array.
@@ -41,17 +54,31 @@ public class Q_01_MaximumValueInArray {
     // write a function which returns maximum value in given array range or say subArray.
     // this function returns the maximum value between given indexes last index included.
 
-    static int maximumValueInRange(int[] array, int fromIndex,int toIndex){
+    static int maximumValueInRange(int[] array, int start,int end){
+        // checking if passed array is empty or not?
         if (array.length == 0) {
             throw new IllegalArgumentException("Array is empty");
         }
-        int maximumValue = array[fromIndex];
 
-        for (int i = fromIndex; i <= toIndex; i++){
-            if(array[i]>maximumValue){
-                maximumValue= array[i];
+        // checking if ranges are given properly
+        if(end < start){
+            throw new IllegalArgumentException("Please check the Indexes You passed, start index should be Smaller then end index.");
+        }
+
+        // if end is out of bound.
+        if (start < 0 || end >= array.length) {
+            throw new IndexOutOfBoundsException("Start or end index is out of array bounds");
+        }
+
+
+        int maximumValue = array[start];
+
+        for (int i = start; i <= end; i++) {
+            if (array[i] > maximumValue) {
+                    maximumValue = array[i];
             }
         }
+
         return maximumValue;
     }
 }
