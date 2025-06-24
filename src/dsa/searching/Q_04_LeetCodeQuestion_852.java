b600712e669e4c0619e5cea11b381c87578f3b65
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2025-06-24 13:53:06 +0530

    Starting Binary Search Questions

---
+++ src/dsa/searching/Q_04_LeetCodeQuestion_852.java
@@ -0,0 +1,26 @@
+package dsa.searching;
+/*
+    Peak index in a Mountain Array.
+
+    let's call an array arr a mountain if the following properties hold.
+    -- array.length>=3
+    -- there exist some i with 0 < i < arr.length -1 such that:
+        * arr[0]< arr[1]< arr[2]<....arr[i-1]< arr[i]
+        * arr[i]> arr[i+1]> arr[2]>.... >arr[arr.length - 1]
+
+    given an integer array arr that is guaranteed to be a mountain, return any i
+    such that arr[0]<arr[1]<......arr[i-1]<arr[i]>arr[i+1]>.....>arr[arr.length-1]
+
+    Example:
+           Input: arr = [0,1,0]
+           output: 1
+
+    link: https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+ */
+
+public class Q_04_LeetCodeQuestion_852 {
+    public static void main(String[] args) {
+
+    }
+}
