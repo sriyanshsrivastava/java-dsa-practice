ff9ba84ac118bba4f4d4391da926906a4fd6151f
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2025-06-26 23:45:40 +0530

    Solving Leetcode Question-852,Peak index in a Mountain Array.

--- src/dsa/searching/Q_04_LeetCodeQuestion_852.java
+++ src/dsa/searching/Q_04_LeetCodeQuestion_852.java
@@ -22,5 +22,24 @@ package dsa.searching;
 public class Q_04_LeetCodeQuestion_852 {
     public static void main(String[] args) {
         // will do it later
+        int[] arr = {0,1,2,8,5,6,4};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+
+    public static int peakIndexInMountainArray(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start < end){
+
+            int mid = start + (end-start) / 2;
+
+            if(arr[mid] > arr[mid+1]){
+                end = mid;
+            } else{
+                start = mid+1; // we know that mid+1 is greater than mid value
+            }
+        }
+        return end;
     }
 }
